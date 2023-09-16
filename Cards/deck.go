b028ca84e582f068456a98b4692b9694e4ff666a
Lazar Stojanović <lakisd98@gmail.com>
2023-09-17 00:17:42 +0200

    Feat: add shuffle deck feature

--- Cards/deck.go
+++ Cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -57,4 +58,11 @@ func newDeckFromFile(filename string) deck {
 	cards := strings.Split(string(bs), "\n")
 
 	return cards
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Make the deck
 	deck := newDeck()
 
 	// Deal the deck
-	hand, _ := deal(5, deck)
+	// hand, _ := deal(5, deck)
 
 	// Write to file
-	hand.writeToFile("currentHand")
+	// hand.writeToFile("currentHand")
 
 	// Read deck from file
-	newDeck := newDeckFromFile("my")
+	// newDeck := newDeckFromFile("currentHand")
 
-	fmt.Println(newDeck)
+	// Shuffle the deck
+	deck.shuffle()
+
+	deck.print()
 }
