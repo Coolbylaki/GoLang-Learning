dfb33db2b648f1d23d24a3c37ef8c7a1036dfe21
Lazar Stojanović <lakisd98@gmail.com>
2023-09-17 03:38:58 +0200

    Feat: add struct data structure

---
+++ Structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email: "test@gmail.com",
+			zip:   12345,
+		},
+	}
+
+	// Pointer
+	alexPointer := &alex
+
+	alexPointer.updateName("Jimmy")
+	alex.printDetails()
+}
+
+func (p person) printDetails() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
